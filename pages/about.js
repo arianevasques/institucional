9deec6f30e806593779b5add64913dda092aa992
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-02-17 19:46:01 -0300

    adiciona páginas home, about e contato

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import styles from "../styles/About.module.css";
+
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      Ola About
+    </div>
+  );
+}
---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Contact.module.css";
+
+
+export default function Contact() {
+  return (
+    <div className={styles.contact}>
+      Ola Contato
+    </div>
+  );
+}
