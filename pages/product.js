4d4dfe026ec236dc6dd1dd95165036c022542239
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-02-18 20:09:39 -0300

    corrige codigo da pagina product

--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,9 @@
-import styles from "../styles/About.module.css";
+import styles from "../styles/Product.module.css";
 import Menu from "../components/menu/menu"
 
-export default function About() {
+export default function Product() {
   return (
-    <div className={styles.about}>
+    <div className={styles.product}>
       <Menu></Menu>
       <h1>Product</h1>
     </div>
