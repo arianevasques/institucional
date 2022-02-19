d56b57ed5637cf513c142fa0fe931bdad74a5e0b
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-02-18 22:59:38 -0300

    adiciona componente BannerHero na pagina home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Menu from "../components/menu/menu"
+import BannerHero from "../components/BannerHero/BannerHero";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <Menu></Menu>
-      <h1>Home</h1>
+      <BannerHero></BannerHero>
     </div>
   );
 }
