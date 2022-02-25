e1fdcac4fdd70c724da1d75dd1f8316c196b113a
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-02-24 22:18:25 -0300

    adiciona container global na pagina about

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,18 @@
 import styles from "../styles/About.module.css";
 import Menu from "../components/menu/menu";
+import Description from "../components/Description/Description";
+import Members from "../components/Members/Members";
 
 export default function About() {
   return (
     <div className={styles.about}>
       <Menu></Menu>
-      <h1>About</h1>
+      <div className={styles.container}>
+        <Description></Description>
+      </div>
+      <div className={styles.container}>
+        <Members></Members>
+      </div>
     </div>
   );
 }
