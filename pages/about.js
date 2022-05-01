540adeec3cdc58d97fe5eef07a667227c2be6e6b
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-05-01 16:05:10 -0300

    adiciona componente Footer a pagina About

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import styles from "../styles/About.module.css";
 import Menu from "../components/menu/menu";
 import Description from "../components/Description/Description";
 import Members from "../components/Members/Members";
+import Footer from "../components/Footer/Footer";
 
 export default function About() {
   return (
@@ -12,6 +13,9 @@ export default function About() {
       </div>
       <div className="container">
         <Members></Members>
+      </div>
+      <div>
+        <Footer></Footer>
       </div>
     </div>
   );
