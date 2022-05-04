2c760db46c0423df012fbe7dc518b0d969e0fd69
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-05-03 23:00:15 -0300

    refatora componente description passando props

--- components/Description/Description.js
+++ components/Description/Description.js
@@ -1,22 +1,15 @@
 import { DescriptionMain, Title, Content } from "../Description/Description.styled";
 
 
-export default function Description() {
+export default function Description(props) {
   return (
     <DescriptionMain>
       <Title>
-        <h2>Sala grande, equipe pequena</h2>
+        <h2>{props.title}</h2>
       </Title>
       <Content>
         <p>
-          Impulsionada por uma equipe de liderança poderosa com mais de 30 anos
-          de experiência em marketing estratégico e design criativo, a Big Room
-          é uma agência de marketing digital de serviço completo construída com
-          base em conhecimento e credibilidade conquistados com muito esforço.
-          Com uma seleção diversificada de clientes em toda a América do Norte e
-          uma coleção de especialistas de nicho para alavancar para cada
-          projeto, podemos nos concentrar em criar a melhor experiência e
-          entregar os melhores resultados.
+          {props.content}
         </p>
       </Content>
     </DescriptionMain>
--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,13 @@ export default function About() {
     <div className={styles.about}>
       <Menu></Menu>
       <div className="container">
-        <Description></Description>
+        <Description
+          title={"Sala grande, equipe pequena"}
+          content={
+            "Impulsionada por uma equipe de liderança poderosa com mais de 30 anos de experiência em marketing estratégico e design criativo, a Big Room é uma agência de marketing digital de serviço completo construída com base em conhecimento e credibilidade conquistados com muito esforço. Com uma seleção diversificada de clientes em toda a América do Norte e uma coleção de especialistas de nicho para alavancar para cada projeto, podemos nos concentrar em criar a melhor experiência e entregar os melhores resultados."
+          }
+          
+        />
       </div>
       <div className="container">
         <Members></Members>
