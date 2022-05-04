e9a38fb60d487776f6ba4ee3649f2ed92dc7720c
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-05-03 23:34:50 -0300

    refatora componente Members passando props e add componente Member

--- components/Members/Members.js
+++ components/Members/Members.js
@@ -1,60 +1,57 @@
 import Image from "next/image";
+import Member from "../Member/Member";
 
 import { MembersMain, Title, BoxMembers, Box } from "../Members/Members.styled";
 
-export default function Members() {
+export default function Members(props) {
   return (
     <MembersMain>
       <Title>
-        <h4>Conheça a equipe</h4>
+        <h4>{props.title}</h4>
       </Title>
-        <BoxMembers>
-          <Box>
-            <Image
-              src="/kurt.jpeg"
-              alt="Foto do membro da empresa"
-              width={325}
-              height={350}
-            />
-            <p>Kurt Cobain - Vocalista do Nirvana e UX/UI</p>
-          </Box>
-          <Box>
-            <Image
-              src="/amy.jpeg"
-              alt="Foto do membro da empresa"
-              width={325}
-              height={350}
-            />
-            <p>Amy Winehouse - Cantora e Social Midia</p>
-          </Box>
-          <Box>
-            <Image
-              src="/bob.jpeg"
-              alt="Foto do membro da empresa"
-              width={325}
-              height={350}
-            />
-            <p>Bob Marley - Rei do reggae e Rh</p>
-          </Box>
-          <Box>
-            <Image
-              src="/janis.jpeg"
-              alt="Foto do membro da empresa"
-              width={325}
-              height={350}
-            />
-            <p>Janis Joplin - Cantora e Marketing</p>
-          </Box>
-          <Box>
-            <Image
-              src="/jimmy.jpeg"
-              alt="Foto do membro da empresa"
-              width={325}
-              height={350}
-            />
-            <p>Jimi Hendrix - Guitarstar e Full Stack Developer </p>
-          </Box>
-        </BoxMembers>
+      <BoxMembers>
+        <Member
+          image={"/kurt.jpeg"}
+          imageAlt={"Foto do membro da empresa"}
+          imageWidth={325}
+          imageHeight={350}
+          name={"Kurt Cobain"}
+          role={"Vocalista do Nirvana e UX/UI"}
+        />
+        <Member
+          image={"/amy.jpeg"}
+          imageAlt={"Foto do membro da empresa"}
+          imageWidth={325}
+          imageHeight={350}
+          name={"Amy Winehouse"}
+          role={"Cantora e Social Midia"}
+        />
+        <Member
+          image={"/bob.jpeg"}
+          imageAlt={"Foto do membro da empresa"}
+          imageWidth={325}
+          imageHeight={350}
+          name={"Bob Marley"}
+          role={"Rei do reggae e Rh"}
+        />
+        <Member
+          image={"/janis.jpeg"}
+          imageAlt={"Foto do membro da empresa"}
+          imageWidth={325}
+          imageHeight={350}
+          name={"Janis Joplin"}
+          role={"Cantora e Marketing"}
+        />
+
+        <Member
+          image={"/jimmy.jpeg"}
+          imageAlt={"Foto do membro da empresa"}
+          imageWidth={325}
+          imageHeight={350}
+          name={"Jimmy Hendrix"}
+          role={"Guitarstar e Full Stack Developer"}
+        />
+      </BoxMembers>
     </MembersMain>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -14,11 +14,12 @@ export default function About() {
           content={
             "Impulsionada por uma equipe de liderança poderosa com mais de 30 anos de experiência em marketing estratégico e design criativo, a Big Room é uma agência de marketing digital de serviço completo construída com base em conhecimento e credibilidade conquistados com muito esforço. Com uma seleção diversificada de clientes em toda a América do Norte e uma coleção de especialistas de nicho para alavancar para cada projeto, podemos nos concentrar em criar a melhor experiência e entregar os melhores resultados."
           }
-          
         />
       </div>
       <div className="container">
-        <Members></Members>
+        <Members
+        title={"Conheça nossa equipe"}
+        />
       </div>
       <div>
         <Footer></Footer>
