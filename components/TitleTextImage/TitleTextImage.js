ad8dea9161fed37c7af211520d678b1c99d4f6fb
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-05-04 21:35:51 -0300

    refatora componente TitleTextImage usando props

--- components/TitleTextImage/TitleTextImage.js
+++ components/TitleTextImage/TitleTextImage.js
@@ -1,41 +1,24 @@
 import {
   TitleTextImageMain,
   TitleText,
-  Image
+  Image,
 } from "../TitleTextImage/TitleTextImage.styled";
 
-export default function TitleTextImage() {
+export default function TitleTextImage(props) {
   return (
     <TitleTextImageMain>
       <TitleText>
-        <h3> B2B</h3>
-        <p>
-          Você não encontrará outra agência com a vasta experiência em marketing
-          B2B que temos. Nós prosperamos em nichos de mercado e entendemos
-          rapidamente a terminologia e os processos de compra complicados. Veja
-          como podemos ajudar sua empresa B2B a aumentar as vendas e o ROI:
-        </p>
-        <h4>Construção de relacionamento personalizado e de longo prazo.</h4>
-        <p>
-          Construir engajamento e transformar oportunidades em clientes pode ser
-          um desafio na indústria B2B. Podemos ajudar a simplificar esse
-          processo e ajudá-lo a criar clientes para toda a vida.
-        </p>
-        <h4>Marketing e comunicação de conteúdo B2B direcionado</h4>
-        <p>
-          Com uma profunda compreensão da jornada do comprador B2B, podemos
-          ajudá-lo a personalizar sua estratégia de conteúdo e alinhar seus
-          esforços para educar, atrair e converter as pessoas certas.
-        </p>
-        <h4>Estratégias digitas orientadas a nichos e sites otimizados</h4>
-        <p>
-          Entender seu público e setor é apenas metade da batalha. Podemos
-          ajudá-lo a identificar palavras-chave, melhorar a visibilidade online
-          e atrair as empresas certas para o seu site.
-        </p>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+        <h4>{props.sectionTitle1}</h4>
+        <p>{props.sectionText1}</p>
+        <h4>{props.sectionTitle2}</h4>
+        <p>{props.sectionText2}</p>
+        <h4>{props.sectionTitle3}</h4>
+        <p>{props.sectionText3}</p>
       </TitleText>
       <Image>
-        <img src="b2b.jpeg"></img>
+        <img src={props.image}></img>
       </Image>
     </TitleTextImageMain>
   );
--- pages/product.js
+++ pages/product.js
@@ -8,7 +8,6 @@ import TextTwoTextColums from "../components/TextTwoTextColums/TextTwoTextColums
 import WhatWeDo from "../components/WhatWeDo/WhatWeDo";
 import Footer from "../components/Footer/Footer";
 
-
 export default function Product() {
   return (
     <div className={styles.product}>
@@ -16,41 +15,68 @@ export default function Product() {
       <div className="container">
         <BigHeader
           title={"Where is Shine"}
-          description={"Soluções de marketing técnicas e funcionais inovadoras para qualquer problema."}
+          description={
+            "Soluções de marketing técnicas e funcionais inovadoras para qualquer problema."
+          }
         />
       </div>
-      <HorizontalBanner image={'nebulosa.jpeg'} />
+      <HorizontalBanner image={"nebulosa.jpeg"} />
       <div className="container">
         <BestFeatures
-        title={"Trazemos marcas para fora das sombras e para a luz"}
-        content={"É raro encontrar profissionais de marketing que falem sua língua e conheçam as nuances do seu setor. Mas na Big Room, esse é o nosso maior patrimônio! Nossa equipe incomparável de profissionais demarketing, designers e desenvolvedores talentosos ajudou clientes de todos os tamanhos e setores a resolver seus desafios de marketing exclusivos e expandir seus negócios. Aqui estão algumas das áreas em que nos especializamos:"}
-        icon1={"icon-b2b-1.jpeg"}
-        icon1Label={"B2B"}
-        icon2={"icon-dynamics-1.jpeg"}
-        icon2Label={"Microsoft Dynamics"}
-        icon3={"icon-business-applications-1.jpeg"}
-        icon3Label={"Aplicativos de negócios"}
-        icon4={"icon-ecommerce-1.jpeg"}
-        icon4Label={"comércio eletrônico"}
+          title={"Trazemos marcas para fora das sombras e para a luz"}
+          content={
+            "É raro encontrar profissionais de marketing que falem sua língua e conheçam as nuances do seu setor. Mas na Big Room, esse é o nosso maior patrimônio! Nossa equipe incomparável de profissionais demarketing, designers e desenvolvedores talentosos ajudou clientes de todos os tamanhos e setores a resolver seus desafios de marketing exclusivos e expandir seus negócios. Aqui estão algumas das áreas em que nos especializamos:"
+          }
+          icon1={"icon-b2b-1.jpeg"}
+          icon1Label={"B2B"}
+          icon2={"icon-dynamics-1.jpeg"}
+          icon2Label={"Microsoft Dynamics"}
+          icon3={"icon-business-applications-1.jpeg"}
+          icon3Label={"Aplicativos de negócios"}
+          icon4={"icon-ecommerce-1.jpeg"}
+          icon4Label={"comércio eletrônico"}
+
         />
       </div>
       <div className="container">
-        <TitleTextImage></TitleTextImage>
+        <TitleTextImage
+          title={"B2B"}
+          content={
+            " Você não encontrará outra agência com a vasta experiência em marketing B2B que temos. Nós prosperamos em nichos de mercado e entendemos rapidamente a terminologia e os processos de compra complicados. Veja como podemos ajudar sua empresa B2B a aumentar as vendas e o ROI:"
+          }
+          sectionTitle1={
+            "Construção de relacionamento personalizado e de longo prazo."
+          }
+          sectionText1={
+            "Construir engajamento e transformar oportunidades em clientes pode ser um desafio na indústria B2B. Podemos ajudar a simplificar esse processo e ajudá-lo a criar clientes para toda a vida."
+          }
+          sectionTitle2={"Marketing e comunicação de conteúdo B2B direcionado"}
+          sectionText2={"Com uma profunda compreensão da jornada do comprador B2B, podemos ajudá-lo a personalizar sua estratégia de conteúdo e alinhar seus esforços para educar, atrair e converter as pessoas certas."}
+          sectionTitle3={"Estratégias digitas orientadas a nichos e sites otimizados"}
+          sectionText3={"Entender seu público e setor é apenas metade da batalha. Podemos ajudá-lo a identificar palavras-chave, melhorar a visibilidade online e atrair as empresas certas para o seu site."}
+          image={"b2b.jpeg"}
+        />
       </div>
-      <div className="container" style={{marginTop: 90}}>
+      <div className="container" style={{ marginTop: 90 }}>
         <WhatWeDo
           title={"Product Variables"}
           moreText={"Mais"}
           headerText={
             "O Google Tradutor é um serviço virtual gratuito da subsidiaria \r\n Google da Alphabet Inc. de tradução instantânea de textos e \r\n websites. A empresa introduziu o software em 2006, inicialmente \r\n suportando apenas os idiomas inglês e árabe."
           }
-          box1Title={'Ttle Box 1'}
-          box2Title={'Ttle Box 2'}
-          box3Title={'Ttle Box 3'}
-          box1Content={"Elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veion ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-          box2Content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolnim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-          box3Content={"Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-          boxMoreText={'Leia Mais'}
+          box1Title={"Ttle Box 1"}
+          box2Title={"Ttle Box 2"}
+          box3Title={"Ttle Box 3"}
+          box1Content={
+            "Elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veion ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+          box2Content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolnim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+          box3Content={
+            "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+          boxMoreText={"Leia Mais"}
         />
       </div>
       <HorizontalBanner></HorizontalBanner>
