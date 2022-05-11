d65e3d0a150de34c58f85a9d5cd083db247c04a5
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-05-10 21:07:01 -0300

    refatora componente Menu passando props

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,29 +1,11 @@
 import { MenuMain } from "../Menu/Menu.styled";
 
 export default function Menu(props) {
-  const retornoDaAPI = [
-    {
-      link: "/",
-      name: "Home",
-    },
-    {
-      link: "/about",
-      name: "Sobre",
-    },
-    {
-      link: "/product",
-      name: "Produto",
-    },
-    {
-      link: "/contact",
-      name: "Contato",
-    },
-  ];
 
   return (
     <MenuMain>
       <ul>
-        {retornoDaAPI.map((item, index) => {
+        {props.data.map((item, index) => {
           return (
             <li key={index}>
               <a href={item.link}>{item.name}</a>
